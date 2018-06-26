88aa8d2a0a2e0a86bc091e149d67d74386b6764f
Michał Kuliś <michalkulis1993@gmail.com>
2018-06-26 15:40:48 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    
+    fetch(url + countryName)
+        .then(function(resp){
+            return resp.json();
+        }) 
+        .then(showCountriesList);
+}
+
+ function showCountriesList(resp) {
+     
+     if(Array.isArray(resp)) {   
+         countriesList.innerHTML='';
+         
+         resp.forEach(function(item){
+             var liElement = document.createElement('li');
+             liElement.innerHTML = '<h3>' + item.name + '</h3>' + '</br>' + 'Capital: ' + item.capital + '</br>' + 'Subregion: ' + item.subregion + '</br>' + 'Languages: ' + item.languages + '</br>' + 'Population: ' + item.population;
+             countriesList.appendChild(liElement);
+         });
+     } else {
+         alert('WE NOT FOUND THIS COUNTRY');
+     }
+ }
+
