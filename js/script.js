b74469df11cda3dc037b1c4230d3fea425f9408f
Michał Kuliś <michalkulis1993@gmail.com>
2018-06-27 01:45:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,27 +7,22 @@ document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
-    if(!countryName.length) countryName = 'Poland';
-    
+    if (!countryName.length) {countryName = 'Poland';}
+
     fetch(url + countryName)
-        .then(function(resp){
+        .then(function (resp) {
             return resp.json();
-        }) 
+        })
         .then(showCountriesList);
 }
 
- function showCountriesList(resp) {
-     
-     if(Array.isArray(resp)) {   
-         countriesList.innerHTML='';
-         
-         resp.forEach(function(item){
-             var liElement = document.createElement('li');
-             liElement.innerHTML = '<h3>' + item.name + '</h3>' + '</br>' + 'Capital: ' + item.capital + '</br>' + 'Subregion: ' + item.subregion + '</br>' + 'Languages: ' + item.languages + '</br>' + 'Population: ' + item.population;
-             countriesList.appendChild(liElement);
-         });
-     } else {
-         alert('WE NOT FOUND THIS COUNTRY');
-     }
- }
+function showCountriesList(resp) {
 
+    if (!Array.isArray(resp)) {alert('WE NOT FOUND THIS COUNTRY');}
+    countriesList.innerHTML = '';
+    resp.forEach(function (item) {
+        var liElement = document.createElement('li');
+        liElement.innerHTML = '<h3>' + item.name + '</h3>' + '</br>' + 'Capital: ' + item.capital + '</br>' + 'Subregion: ' + item.subregion + '</br>' + 'Languages: ' + item.languages + '</br>' + 'Population: ' + item.population;
+        countriesList.appendChild(liElement);
+    });
+}
